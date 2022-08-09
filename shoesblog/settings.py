7755c56fe762047e4bb43740b53f3ffab18db388
diegoh22 <diegohofficial@gmail.com>
2022-08-09 15:16:06 +0000

    final commit

--- shoesblog/settings.py
+++ shoesblog/settings.py
@@ -28,7 +28,9 @@ TEMAPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['shoesblog.herokuapp.com', 'localhost']
 
